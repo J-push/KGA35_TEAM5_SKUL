27616c4fd28f5dd3280598b088fdf36d7cd557e0
Justpush <a85429462@gmail.com>
2022-05-03 10:46:18 +0900

    [Update] [2022.05.03 박지용] : Insert TileMap.cpp, Header
    타일맵 헤더와 cpp파일 추가

---
+++ Skul/Tilemap.cpp
@@ -0,0 +1,59 @@
+#include "Tilemap.h"
+
+Tilemap::Tilemap()
+{
+	normalGround.width = GROUND_WIDTH;
+	normalGround.height = GROUND_HEIGHT;
+}
+
+void Tilemap::CreateGrass()
+{
+}
+
+void Tilemap::CreateWall()
+{
+}
+
+int Tilemap::CreateBackGround()
+{
+	int cols = normalGround.width / TILE_SIZE;
+	int rows = normalGround.height / TILE_SIZE;
+
+	tileMap.setPrimitiveType(Quads);
+	tileMap.resize(cols * rows * VERTS_IN_QUAD);
+
+	int index = 0;
+
+	for (int r = 0; r < rows; ++r)
+	{
+		for (int c = 0; c < cols; ++c)
+		{
+			int index = r * cols + c;
+
+			float x = c * TILE_SIZE;
+			float y = r * TILE_SIZE;
+
+			int vertexIndex = index * VERTS_IN_QUAD;
+
+			tileMap[vertexIndex + 0].position = Vector2f(x, y);
+			tileMap[vertexIndex + 1].position = Vector2f(x + TILE_SIZE, y);
+			tileMap[vertexIndex + 2].position = Vector2f(x + TILE_SIZE, y + TILE_SIZE);
+			tileMap[vertexIndex + 3].position = Vector2f(x, y + TILE_SIZE);
+
+			int texIndex = 
+			if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1)
+			{
+				texIndex = 3;
+			}
+			float offset = texIndex * TILE_SIZE;
+			tileMap[vertexIndex + 0].texCoords = Vector2f(0.f, offset);
+			tileMap[vertexIndex + 1].texCoords = Vector2f(TILE_SIZE, offset);
+			tileMap[vertexIndex + 2].texCoords = Vector2f(TILE_SIZE, offset + TILE_SIZE);
+			tileMap[vertexIndex + 3].texCoords = Vector2f(0.f, offset + TILE_SIZE);
+		}
+	}
+}
+
+Tilemap::~Tilemap()
+{
+}
---
+++ Skul/Tilemap.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+#include "Utils.h"
+#include "SFML/System.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
+enum class TileType
+{
+	Null,
+	Wall,
+	Ground,
+	HalfGround,
+};
+
+/******************************************************************************
+* 설명 : 타일마다 뭐 부딪히는지
+*******************************************************************************/
+enum class ColliderTag
+{
+	None,
+	Ground,
+	HalfGround,
+	Player,
+	Monster,
+	Exit,
+	NPC,
+	RayCast,
+	PlayerAttack,
+	MonsterAttack,
+};
+
+using namespace sf;
+class Tilemap
+{
+private:
+	const int TILE_SIZE = 32;
+	const int TILE_TYPES = 4;
+	const int VERTS_IN_QUAD = 4;
+	const int GROUND_WIDTH = 3200;
+	const int GROUND_HEIGHT = 3200;
+	sf::VertexArray tileMap;
+	sf::IntRect normalGround;
+
+public:
+	Tilemap();
+	void CreateGrass();
+	void CreateWall();
+	int CreateBackGround();
+	virtual ~Tilemap();
+};
+
