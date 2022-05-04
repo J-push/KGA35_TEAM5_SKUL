f430a2c4ae71d1f0d1c716c330fa2c548f86e750
DESKTOP-BFVSLI0\KGA_07 <huy1224@naver.com>
2022-05-04 10:32:12 +0900

    [Update][2022.05.04] 마우스클래스

--- Skul/Source/ChangeMouse.h
+++ Skul/Source/ChangeMouse.h
@@ -2,6 +2,9 @@
 
 #include "../Source/Manager/InputManager.h"
 #include "../Source/Manager/ResourceMgr.h"
+#include "../Source/Utils/Utils.h"
+
+#include <iostream>
 
 
 class MouseCursor
@@ -11,14 +14,20 @@ private:
 	FloatRect CursorBounds;
 
 public:
-	MouseCursor()
+	void Init()
 	{
 		spriteCursor.setTexture(*ResourceMgr::instance()->GetTexture("CURSORTEX"));
+
+		//Utils::SetOrigin(spriteCursor, Pivots::CC);
+
 	}
 
 	void Update(float dt)
 	{
 		spriteCursor.setPosition(InputManager::GetMouseWorldPosition());
+		spriteCursor.setPosition().x = 100;
+
+		std::cout << spriteCursor.getPosition().x;
 	}
 
 	virtual void Draw(sf::RenderWindow *window)
